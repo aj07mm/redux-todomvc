9400c469570f6fae94551af1b3b9c3a822d8f900
Julio Marins <julio.marins@esharesinc.com>
2016-10-09 12:40:36 -0700

    adding vision to REDUX_DEVTOOLS_EXTENSION

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 
-const store = createStore(reducer)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+//export const createAsyncStore = compose(
+//    applyMiddleware(thunkMiddleware),
+//    window.devToolsExtension ? window.devToolsExtension() : f => f
+//)(createStore);
 
 render(
   <Provider store={store}>
