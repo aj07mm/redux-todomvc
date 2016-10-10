dcb20f7e319396d068a831c1f865b2f3ec33d16f
Julio Marins <julio.marins@esharesinc.com>
2016-10-09 17:10:11 -0700

    redux using redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, compose } from 'redux'
+import { createStore, compose, combineReduxers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
+import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-//export const createAsyncStore = compose(
+// ADDING REDUX-TUNK AS MIDDLEWARE
+//const createAsyncStore = compose(
 //    applyMiddleware(thunkMiddleware),
 //    window.devToolsExtension ? window.devToolsExtension() : f => f
-//)(createStore);
+//)(createStore); // 1st param middleware, 2nd param extension
+
+// STORE INITIAL STATE
+//const ajaxInitialState = {
+//    rowData: [],
+//    isLoading: true,
+//    num_pages: 1,
+//    count: 0,
+//    queryOpts: {
+//        filters: null,
+//        searchTerm: '', // Should be an empty string for text input.
+//        sortKey: null,
+//        page: 1,
+//        pageSize: 50,
+//    },
+//    newRows: [],
+//    deletedRows: [],
+//};
+
+// REDUCER
+//const function ajaxReducer(state = ajaxInitialState, action) {
+//    switch (action.type) {
+//        case SHOW_LOADING:
+//            return {
+//                ...state,
+//                isLoading: true
+//            };
+//        case LOAD_DATA:
+//            return {
+//                ...state,
+//                isLoading: false,
+//                rowData: action.data.results,
+//                num_pages: action.data.num_pages,
+//                count: action.data.count
+//            };
+
+// REDUX STORE GENERATOR
+//const configureStore = (initialState) => (
+//    createAsyncStore( // createStore is inside
+//        combineReducers({ //return combine reducers
+//            ajaxReducer: ajaxReducer,
+//            modal: modalReducer,
+//            radio: radioButtonsReducer
+//        }),
+//        {
+//            ajaxReducer: initialState,
+//            modal: modalInitialState,
+//            radio: radioButtonsReducer
+//        }
+//    )
+//)
+
+//const store = configureStore(ajaxInitialState);
+
+// the same as createAsyncStore but without thunk middleware
+//const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(reducer, compose(
+        applyMiddleware(thunkMiddleware), // enhancer
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // DevTools
+    )
+);
 
 render(
   <Provider store={store}>
